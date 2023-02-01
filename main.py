b56670b3cd7b74367ac457aa0dd09baf32c657c2
chicken10dys <chicken@10dys.ca>
2023-02-01 00:42:31 -0500

    shows more info

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests, json, os
 
-# check if json exists and if not create it
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 endpoint = 'https://api.openweathermap.org/data/2.5/weather'
 running = True
+
+# check if json exists and if not create it
 try:
     with open('config.json', 'r') as file:
         config = json.load(file)
@@ -55,17 +59,34 @@ while running == True:
 
             if 'main' in weatherData:
                 # Access specific weatherData points
+                name = weatherData['name']
+                description = weatherData['weather'][0]['description']
                 temperature = weatherData['main']['temp']
+                feels = weatherData['main']['feels_like']
                 humidity = weatherData['main']['humidity']
 
-                print("\n")
+                clear()
+
+                print(f'{name}')
+                print(f'{description}')
+                print("")
                 if (int)(temperature) <= 0:
                     print(f'Temperature: {temperature}°C' + " ❄️❄️❄️")
                 elif (int)(temperature) > 0:
                     print(f'Temperature: {temperature}°C' + " ☀️☀️☀️")
                 else:
                     print(f'Temperature: {temperature}°C')
+
+                if (int)(feels) <= 0:
+                    print(f'Feels like: {feels}°C' + " ❄️❄️❄️")
+                elif (int)(feels) > 0:
+                    print(f'Feels like: {feels}°C' + " ☀️☀️☀️")
+                else:
+                    print(f'Feels like: {feels}°C')
+
+                print("")
                 print(f'Humidity: {humidity}%')
+
             else:
                 print("City not found.")
         else:
